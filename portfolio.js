197c37a5494a3f24f042d601ae46d74c049c8d15
Mohdhar Noor <mohdhar.noor@gmail.com>
2014-06-30 11:09:30 -0400

    Added individual portfolio items without a description

--- portfolio.js
+++ portfolio.js
@@ -2,9 +2,11 @@ jQuery(document).ready(function(){
 	//mouseenter highlight here
 	$('.expand').on('mouseenter', function(){
 		$(this).addClass('highlight');
+		$(this).children().show();
 	});
 
 	$('.expand').on('mouseleave', function(){
+		$(this).children().hide();
 		$(this).removeClass('highlight');
 	});
 });
