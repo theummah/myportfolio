e3c1fb0317e625511fdd88e2d8fafc6e535927b0
Mohdhar Noor <mohdhar.noor@gmail.com>
2014-07-02 00:58:08 -0400

    Added arrow and contact icon

--- portfolio.js
+++ portfolio.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
 
+	$("#partzero").css('height', $(window).height());
+	$("#partone").css('height', $(window).height());
+	$("#parttwo").css('height', $(window).height());
+	$("#partthree").css('height', $(window).height());
 });
 
 
 $(document).on('click', '.myexp', function(){
-	$('#list_edu').fadeToggle();
+	$(this).find('.list_edu').fadeToggle();
 });
 	$(document).on('click', '#arrow_container', function(){
        $('html, body').stop().animate({
